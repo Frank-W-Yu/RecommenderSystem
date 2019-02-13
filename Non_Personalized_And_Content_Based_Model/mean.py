c8d40bb5c6928bc5c09b8d31affe27971ba76a76
Frank Yu <Frank.Yu3@wdc.com>
2019-02-12 17:54:16 -0800

    simple_mean_C1

--- Non_Personalized_And_Content_Based_Model/mean.py
+++ Non_Personalized_And_Content_Based_Model/mean.py
@@ -11,17 +11,13 @@ def load_data(file_name, header_lines):
     return data_set
 
 def simple_mean(file_name):
-    # file_name = r'/home/rma_lab/frank/Recommender/RecommenderSystem/Non_Personalized_And_Content_Based_Model/data/ratings.csv'
     ld = load_data(file_name, 1)
     ld_next = ld.make_one_shot_iterator().get_next()
-    # movie_rating = tf.contrib.lookup.MutableHashTable(key_dtype=tf.float32, value_dtype=tf.float32, default_value=-1)
     movie_rating = {}
     users = {}
-    #means = tf.contrib.lookup.HashTable(key_dtype=tf.int32, value_dtype=tf.int32, default_value=-1)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         try:
-            # for i in range(10):
             i = j = 1
             while True:
                 if i == 10000:
@@ -44,15 +40,51 @@ def simple_mean(file_name):
             print('Done')
         for movie_id, rating in movie_rating.items():
             movie_rating[movie_id] = rating / users[movie_id]
-        movie_ids = tf.convert_to_tensor(list(movie_rating.keys()))
-        ratings = tf.convert_to_tensor(list(movie_rating.values()))
-        table = tf.contrib.lookup.HashTable(tf.contrib.lookup.KeyValueTensorInitializer(movie_ids, ratings), -1)
-        table.init.run()
-        print('table size')
-        print(table.size().eval())
-        out = table.export()
-        print(out.eval())
-    return out
+    return movie_rating
+
+def dampled_mean(file_name):
+    ld = load_data(file_name, 1)
+    ld_next = ld.make_one_shot_iterator().get_next()
+    itemSumRating = {}
+    itemCountRating = {}
+    movie_rating = {}
+    users = {}
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        try:
+            i = j = 1
+            while True:
+                if i == 10000:
+                    print(j)
+                    j += 1
+                    i = 0
+                i += 1
+                data_line = sess.run(ld_next)
+                userId = int(data_line[0])
+                movieId = int(data_line[1])
+                rating = data_line[2]
+                if movieId not in movie_rating:
+                    movie_rating[movieId] = rating
+                    users[movieId] = 1
+                else:
+               
+                    movie_rating[movieId] = movie_rating[movieId] + rating
+                    users[movieId] = users[movieId] + 1
+        except tf.errors.OutOfRangeError:
+            print('Done')
+        for movie_id, rating in movie_rating.items():
+            movie_rating[movie_id] = rating / users[movie_id]
+    return movie_rating
+
+def recommendItems(numOfRec, movie_rating):
+    results = []
+    for k, v in movie_rating.items():
+        results.append((k, v))
+    results.sort(key = lambda x : -x[1])
+    return results[:numOfRec]
 
 if __name__ == '__main__':
-    simple_mean('data/ratings.csv')
+    movie_rating = simple_mean(r'data/ratings.csv')
+    results = recommendItems(10, movie_rating)
+    for k, v in results:
+        print(k, ':', v)
